ad4a85e967bfdbf2d992f887784c5496abff1470
chrisjhkim <chris1108g@gmail.com>
2023-10-27 23:17:02 +0900

    Initial commit

---
+++ src/main/java/com/chris1108/articlefilter/ArticlefilterApplication.java
@@ -0,0 +1,13 @@
+package com.chris1108.articlefilter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArticlefilterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArticlefilterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chris1108/articlefilter/ArticlefilterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chris1108.articlefilter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArticlefilterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
